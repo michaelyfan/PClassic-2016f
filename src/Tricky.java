ef61607b0321d488194eb5ca39b950f3a92bdc67
michael.fan <mchl_fan@yahoo.com>
2016-11-27 00:39:45 -0500

    Added Tricky (problem 5)

--- src/Tricky.java
+++ src/Tricky.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Tricky {
     public static void main(String[] args) throws FileNotFoundException {
         // Before submitting, make sure the main method hasn't been changed!
-        Scanner sc = new Scanner(new FileReader("TrickyIN.txt"));
+        Scanner sc = new Scanner(new FileReader("testcases/TrickyIN.txt"));
         while(sc.hasNext()) {
             int length = sc.nextInt();
             Iterable<Integer> output = findTriples(length);
@@ -25,28 +25,19 @@ public class Tricky {
     }
     
     public static Iterable<Integer> findTriples(int hypotenuse) {
-        Iterable<Integer> list = new ArrayList<Integer>();
-        Iterable<Integer> list2 = new ArrayList<Integer>();
-        for(int i=3; i<hypotenuse/Math.sqrt(2); i++)
-        {
-         int x = (int) Math.sqrt(Math.pow(hypotenuse, 2)-Math.pow(i, 2));
-         	if(Math.pow(x, 2)+Math.pow(i, 2)==Math.pow(hypotenuse, 2))
-         	{
-         		((ArrayList<Integer>) list).add(i);
-         		((ArrayList<Integer>) list).add(x);
-         		((ArrayList<Integer>) list2).add(0, i);
-         		((ArrayList<Integer>) list2).add(0, x);
-         	}
+    	ArrayList<Integer> toReturn = new ArrayList<Integer>();
+        for (int a = 1; a < hypotenuse; a++) {
+        	for (int b = 1; b < a; b++) {
+        		if (a*a+b*b==hypotenuse*hypotenuse) {
+        			toReturn.add(a);
+        			toReturn.add(b);
+        		}
+        	}
         }
-        Iterable<Integer> finalList = new ArrayList<Integer>();
-        for (int i=0; i<((ArrayList<Integer>) list).size(); i++)
-        {
-        	((ArrayList<Integer>) finalList).add(((ArrayList<Integer>) list).get(i));
+        for (int k = 0; k < toReturn.size(); k++) {
+        	toReturn.add(0, toReturn.get(k));
+        	k++;
         }
-        for (int i=0; i<((ArrayList<Integer>) list2).size(); i++)
-        {
-        	((ArrayList<Integer>) finalList).add(((ArrayList<Integer>) list2).get(i));
-        }
-        return finalList;
+        return toReturn;
     }
 }
